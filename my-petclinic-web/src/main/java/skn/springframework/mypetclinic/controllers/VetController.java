cb454e462b3d898f2e9acb4c478118c802397aa7
nightsaken <antuan555@yandex.ru>
2024-10-25 18:32:27 +0300

    fixing broken links, closes #46

--- my-petclinic-web/src/main/java/skn/springframework/mypetclinic/controllers/VetController.java
+++ my-petclinic-web/src/main/java/skn/springframework/mypetclinic/controllers/VetController.java
@@ -14,7 +14,7 @@ public class VetController {
         this.vetService = vetService;
     }
 
-    @RequestMapping({"","/","/index","/index.html"})
+    @RequestMapping({"","/","/index","/index.html", "/vets.html"})
     public String listVets(Model model){
         model.addAttribute("vets", vetService.findAll());
         return "vets/index";
