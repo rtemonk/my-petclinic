c02100b0d0baf39fa5d0c748cf958a8f7d1746f0
nightsaken <antuan555@yandex.ru>
2024-10-25 17:38:17 +0300

    adding specialty map impl, closes #32

---
+++ my-petclinic-data/src/main/java/skn/springframework/mypetclinic/services/SpecialtyService.java
@@ -0,0 +1,6 @@
+package skn.springframework.mypetclinic.services;
+
+import skn.springframework.mypetclinic.model.Specialty;
+
+public interface SpecialtyService extends CrudService<Specialty,Long> {
+}
