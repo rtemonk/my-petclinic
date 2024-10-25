f217530db07685ee099d640bb3f41730ba6f6b9a
nightsaken <antuan555@yandex.ru>
2024-10-25 16:43:29 +0300

    adding pet type service, closes #30

---
+++ my-petclinic-data/src/main/java/skn/springframework/mypetclinic/services/PetTypeService.java
@@ -0,0 +1,7 @@
+package skn.springframework.mypetclinic.services;
+
+import skn.springframework.mypetclinic.model.PetType;
+
+public interface PetTypeService extends CrudService<PetType, Long>{
+
+}
---
+++ my-petclinic-data/src/main/java/skn/springframework/mypetclinic/services/map/PetTypeServiceMap.java
@@ -0,0 +1,33 @@
+package skn.springframework.mypetclinic.services.map;
+
+import org.springframework.stereotype.Service;
+import skn.springframework.mypetclinic.model.PetType;
+import skn.springframework.mypetclinic.services.PetTypeService;
+
+import java.util.Set;
+
+@Service
+public class PetTypeServiceMap extends AbstractMapService<PetType,Long> implements PetTypeService {
+    @Override
+    public Set<PetType> findAll() {
+        return super.findAll();
+    }
+    @Override
+    public PetType findById(Long id) {
+        return super.findById(id);
+    }
+    @Override
+    public PetType save(PetType obj) {
+        return super.save(obj);
+    }
+    @Override
+    public void delete(PetType obj) {
+        super.delete(obj);
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        super.deleteById(id);
+    }
+
+}
