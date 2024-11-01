66c9f7d11de9bd8e63231ca97fd7cf3e9b0ac2ae
nightsaken <antuan555@yandex.ru>
2024-11-02 02:49:46 +0300

    push to latest commit, made a mistake

---
+++ my-petclinic-data/src/main/java/skn/springframework/mypetclinic/services/springdatajpa/OwnerServiceJPA.java
@@ -0,0 +1,60 @@
+package skn.springframework.mypetclinic.services.springdatajpa;
+
+import org.springframework.context.annotation.Profile;
+import org.springframework.stereotype.Service;
+import skn.springframework.mypetclinic.model.Owner;
+import skn.springframework.mypetclinic.repositories.OwnerRepository;
+import skn.springframework.mypetclinic.repositories.PetRepository;
+import skn.springframework.mypetclinic.repositories.PetTypeRepository;
+import skn.springframework.mypetclinic.services.OwnerService;
+
+import java.util.HashSet;
+import java.util.Set;
+
+@Service
+@Profile("springdatajpa")
+public class OwnerServiceJPA implements OwnerService {
+
+    private final OwnerRepository ownerRepository;
+    private final PetRepository petRepository;
+    private final PetTypeRepository petTypeRepository;
+
+    public OwnerServiceJPA(OwnerRepository ownerRepository, PetRepository petRepository,
+                           PetTypeRepository petTypeRepository) {
+        this.ownerRepository = ownerRepository;
+        this.petRepository = petRepository;
+        this.petTypeRepository = petTypeRepository;
+    }
+
+    @Override
+    public Set<Owner> findAll() {
+        Set<Owner> owners = new HashSet<>();
+        ownerRepository.findAll().forEach(owners::add);
+        return owners;
+    }
+
+    @Override
+    public Owner findById(Long id) {
+        return ownerRepository.findById(id).orElse(null);
+    }
+
+    @Override
+    public Owner save(Owner object) {
+        return ownerRepository.save(object);
+    }
+
+    @Override
+    public void delete(Owner object) {
+        ownerRepository.delete(object);
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        ownerRepository.deleteById(id);
+    }
+
+    @Override
+    public Owner findByLastName(String lastName) {
+        return ownerRepository.findByLastName(lastName);
+    }
+}
