c0d4182d5708fc79be0f8c06d682c48d6b8bc29e
nightsaken <antuan555@yandex.ru>
2024-11-02 01:48:32 +0300

    updated vet to use jpa, closes #38

--- my-petclinic-data/src/main/java/skn/springframework/mypetclinic/model/Specialty.java
+++ my-petclinic-data/src/main/java/skn/springframework/mypetclinic/model/Specialty.java
@@ -1,6 +1,13 @@
 package skn.springframework.mypetclinic.model;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "specialties")
 public class Specialty extends BaseEntity {
+    @Column(name = "description")
     private String description;
 
     public String getDescription() {
