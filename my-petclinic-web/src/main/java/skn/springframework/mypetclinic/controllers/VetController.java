acfd86de227b4b82ac940dc3cc3dde7d14bf679f
nightsaken <antuan555@yandex.ru>
2024-12-16 00:01:08 +0300

    added json endpoint, closes #66

--- my-petclinic-web/src/main/java/skn/springframework/mypetclinic/controllers/VetController.java
+++ my-petclinic-web/src/main/java/skn/springframework/mypetclinic/controllers/VetController.java
@@ -2,8 +2,13 @@ package skn.springframework.mypetclinic.controllers;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import skn.springframework.mypetclinic.model.Vet;
 import skn.springframework.mypetclinic.services.VetService;
+
+import java.util.Set;
 
 @Controller
 @RequestMapping("/vets")
@@ -18,5 +23,10 @@ public class VetController {
     public String listVets(Model model){
         model.addAttribute("vets", vetService.findAll());
         return "vets/index";
+    }
+
+    @GetMapping ("/json")
+    public @ResponseBody Set<Vet> getVetsJson() {
+        return vetService.findAll();
     }
 }
